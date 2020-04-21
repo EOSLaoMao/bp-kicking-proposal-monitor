d06512009cee98f3401556c1e05f855f8a5f72f8
ZhaoYu <zhaoyu.johnny@gmail.com>
2020-04-21 20:29:00 +0900

    sentry added

--- config.js
+++ config.js
@@ -4,6 +4,7 @@ const PROPOSER_ACCOUNT = 'laomaoserver'
 const PROPOSER_PRIVATE_KEY = process.env.PROPOSER_PRIVATE_KEY
 const SLACK_WEBHOOK_URL = process.env.SLACK_WEBHOOK_URL
 const ENABLE_SLACK = process.env.ENABLE_SLACK
+const SENTRY_DSN = process.env.SENTRY_DSN
 const BP_ACCOUNT = 'eoslaomaocom'
 const BP_PERMISSION = [
   {'actor': 'eoslaomao.bp', 'permission': 'active'},
